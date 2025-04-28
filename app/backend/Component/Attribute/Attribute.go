2357156070ab0bc395977d0dc6943df5abe3c147
DF <dragonflybaby2.0@gmail.com>
2025-04-28 15:48:40 +0800

    Enhance: Add Attribute method implements

--- app/backend/Component/Attribute/Attribute.go
+++ app/backend/Component/Attribute/Attribute.go
@@ -5,3 +5,71 @@ type Attribute struct {
 	size    int
 	style   TextStyle
 }
+
+func (att *Attribute) GetContent() string {
+	return att.content
+}
+
+func (att *Attribute) SetContent(content string) {
+	att.content = content
+}
+
+func (att *Attribute) GetSize() int {
+	return att.size
+}
+
+func (att *Attribute) SetSize(size int) {
+	att.size = size
+}
+
+func (att *Attribute) GetStyle() TextStyle {
+	return att.style
+}
+
+func (att *Attribute) SetStyle(style TextStyle) {
+	att.style = style
+}
+
+func (att *Attribute) SetBold(value bool) {
+	if value {
+		att.style |= bold // Set the bold bit
+	} else {
+		att.style &^= bold // Clear the bold bit
+	}
+}
+
+func (att *Attribute) SetItalic(value bool) {
+	if value {
+		att.style |= italic // Set the italic bit
+	} else {
+		att.style &^= italic // Clear the italic bit
+	}
+}
+
+func (att *Attribute) SetUnderline(value bool) {
+	if value {
+		att.style |= underline // Set the underline bit
+	} else {
+		att.style &^= underline // Clear the underline bit
+	}
+}
+
+func (att *Attribute) IsBold() bool {
+	return att.style&bold != 0
+}
+
+func (att *Attribute) IsItalic() bool {
+	return att.style&italic != 0
+}
+
+func (att *Attribute) IsUnderline() bool {
+	return att.style&underline != 0
+}
+
+func (att *Attribute) Copy() *Attribute {
+	return &Attribute{
+		content: att.content,
+		size:    att.size,
+		style:   att.style,
+	}
+}
