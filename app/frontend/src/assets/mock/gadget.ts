834077042fc753c73156c925e25b58dd73e2007c
vincent <hankggg2003xd@gmail.com>
2025-05-14 03:40:25 +0800

    feat(frontend): remove jointjs diagram
    Co-authored-by: Ntut_Garry <garry911128@users.noreply.github.com>

--- app/frontend/src/assets/mock/gadget.ts
+++ app/frontend/src/assets/mock/gadget.ts
@@ -1,69 +1,71 @@
-// src/data/mock_data.ts
-import { BackendCanvasProps } from '../../utils/createCanvas';
+export {};
+// WIP
+// // src/data/mock_data.ts
+// import { BackendCanvasProps } from '../../utils/createCanvas';
 
-const mockData: BackendCanvasProps = {
-  margin: 10,
-  color: 123456,
-  lineWidth: 2,
-  gadgets: [
-    {
-      gadgetType: "Class",
-      x: 100,
-      y: 100,
-      layer: 1,
-      width: 200,
-      height: 150,
-      color: "#3498db",
-      attributes: [
-        [
-          {
-            content: "User",
-            height: 30,
-            width: 200,
-            fontSize: 16,
-            fontStyle: 0,
-            fontFile: "Inkfree",
-          }
-        ],
-        [
-          {
-            content: "name: String",
-            height: 20,
-            width: 180,
-            fontSize: 30,
-            fontStyle: 0,
-            fontFile: "Inkfree",
-          },
-          {
-            content: "email: String",
-            height: 20,
-            width: 180,
-            fontSize: 50,
-            fontStyle: 1,
-            fontFile: "Inkfree",
-          }
-        ],
-        [
-          {
-            content: "+getName(): String",
-            height: 20,
-            width: 180,
-            fontSize: 12,
-            fontStyle: 0,
-            fontFile: "Inkfree",
-          },
-          {
-            content: "+setEmail(email: String): void",
-            height: 20,
-            width: 180,
-            fontSize: 12,
-            fontStyle: 2,
-            fontFile: "Inkfree",
-          }
-        ]
-      ]
-    }
-  ]
-};
+// const mockData: BackendCanvasProps = {
+//   margin: 10,
+//   color: 123456,
+//   lineWidth: 2,
+//   gadgets: [
+//     {
+//       gadgetType: "Class",
+//       x: 100,
+//       y: 100,
+//       layer: 1,
+//       width: 200,
+//       height: 150,
+//       color: "#3498db",
+//       attributes: [
+//         [
+//           {
+//             content: "User",
+//             height: 30,
+//             width: 200,
+//             fontSize: 16,
+//             fontStyle: 0,
+//             fontFile: "Inkfree",
+//           }
+//         ],
+//         [
+//           {
+//             content: "name: String",
+//             height: 20,
+//             width: 180,
+//             fontSize: 30,
+//             fontStyle: 0,
+//             fontFile: "Inkfree",
+//           },
+//           {
+//             content: "email: String",
+//             height: 20,
+//             width: 180,
+//             fontSize: 50,
+//             fontStyle: 1,
+//             fontFile: "Inkfree",
+//           }
+//         ],
+//         [
+//           {
+//             content: "+getName(): String",
+//             height: 20,
+//             width: 180,
+//             fontSize: 12,
+//             fontStyle: 0,
+//             fontFile: "Inkfree",
+//           },
+//           {
+//             content: "+setEmail(email: String): void",
+//             height: 20,
+//             width: 180,
+//             fontSize: 12,
+//             fontStyle: 2,
+//             fontFile: "Inkfree",
+//           }
+//         ]
+//       ]
+//     }
+//   ]
+// };
 
-export default mockData;
+// export default mockData;
